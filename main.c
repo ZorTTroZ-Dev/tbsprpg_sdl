778fd9e656e7c3fd7990bcd66101c1226b26f0e1
cdvanhorn <cdavid.vanhorn@gmail.com>
2024-07-26 21:32:55 -0400

    feature/mem_manager
    Applied linux kernel clang-format to main.

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 
 #include "utilities/mem_manager.h"
 
-int main(void) {
-        struct mmgr_handle *handle = malloc(sizeof(struct mmgr_handle));
-        handle->handle = 42;
-        printf("%d\n", handle->handle);
-        free(handle);
-        mmgr_alloc();
-        printf("Hello World!\n");
-        return 0;
+int main(void)
+{
+	struct mmgr_handle *handle = malloc(sizeof(struct mmgr_handle));
+	handle->handle = 42;
+	printf("%d\n", handle->handle);
+	free(handle);
+	mmgr_alloc();
+	printf("Hello World!\n");
+	return 0;
 }
