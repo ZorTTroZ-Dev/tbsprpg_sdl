e13bdd28ce2633b277015c23189bea1837b86551
cdvanhorn <cdavid.vanhorn@gmail.com>
2024-07-26 22:31:44 -0400

    feature/mem_manager
    Added google test integration, with a very basic test.  Added editor settings folder .idea to .gitignore.

---
+++ google_tests/utilities/mem_manager/mem_pool_tests.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file mem_pool_tests.cpp
+ */
+
+#include <gtest/gtest.h>
+extern "C" {
+	#include "../../../utilities/mem_manager.h"
+}
+
+TEST(MemPoolTests, Initialization) {
+	// Expect two strings not to be equal.
+	// EXPECT_STRNE("hello", "world");
+	// Expect equality.
+	EXPECT_EQ(mmgr_pool_init(), 8);
+}
+
