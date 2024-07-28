fe1c6b0093b6c04a37d068ca6902742f3ed3d431
cdvanhorn <cdavid.vanhorn@gmail.com>
2024-07-27 22:18:41 -0400

    feature/mem_manager
    Started definition of ini value retrieval functions.

--- utilities/ini_config/ini_config.c
+++ utilities/ini_config/ini_config.c
@@ -8,7 +8,7 @@
 /**
  * @brief Open the default config ini file, parse the file and place it in
  *	memory for access.
- * @see inicfg_close()
+ * @see inicfg_close
  * @return int - 0 on success 1 on failure
  */
 int inicfg_open()
@@ -17,8 +17,36 @@ int inicfg_open()
 }
 
 /**
+ * @brief Retrieve the value for the given key in the given section
+ *	will point the value parameter to the key value, do not free
+ *	the given value pointer.
+ * @param section name of section to find key in
+ * @param key name of key to get value for
+ * @param value pointer to char that will contain the requested value
+ * @return int - 0 on success 1 on failure
+ */
+int inicfg_getstring(char *section, char *key, char *value)
+{
+	return FUNC_SUCCESS;
+}
+
+/**
+* @brief Retrieve the value for the given key in the given section
+ *	will point the value parameter to the key value, do not free
+ *	the given value pointer.
+ * @param section name of section to find key in
+ * @param key name of key to get value for
+ * @param value pointer to int that will contain requested value
+ * @return int - 0 on success 1 on failure
+ */
+int inicfg_getint(char *section, char *key, int *value)
+{
+	return FUNC_SUCCESS;
+}
+
+/**
  * @brief Free memory allocated from opening the default config ini file
- * @see inicfg_open()
+ * @see inicfg_open
  * @return int - 0 on success 1 on failure
  */
 int inicfg_close()
