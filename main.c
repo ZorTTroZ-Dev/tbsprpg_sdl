7d0aa89cea243fdbba8bf8c93ff67f0b30de8082
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2024-08-11 18:59:15 -0400

    feature/SDL (#2)
    * feature/sdl
    
    Built win64 and linux amd64 version static of SDL2 library and copied to the lib directory of the project. Updated cmake to include correct version of the library based on build options.
    
    * feature/sdl
    
    Got basic SDL initialization working in Windows and Linux.
    
    * feature/sdl
    
    Window works in windows there is a mesa error in Linux using WSL2.

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include "utilities/ini_config.h"
-
 #include "utilities/logger.h"
-#include "utilities/mem_manager.h"
 
-int main(void)
+#include <stddef.h>
+#include <SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char *argv[])
 {
 	if (log_open(LOG_DEBUG) > 0) {
 		return 1;
@@ -13,19 +19,50 @@ int main(void)
 
 	inicfg_open();
 
-	// uint32_t sizes[] = { 4 };
-	// uint32_t counts[] = { 3 };
-	// struct mmgr_pool_cfg pool_cfg;
-	// pool_cfg.num_pools = 1;
-	// pool_cfg.chunk_sizes = sizes;
-	// pool_cfg.chunk_counts = counts;
-	// mmgr_pool_open(&pool_cfg);
-	//
-	// int *alloc_int = plalloc(sizeof(int));
-	// *alloc_int = 7;
-	// plfree(alloc_int);
-	//
-	// mmgr_pool_close();
+	SDL_Window *window = NULL;
+	SDL_Surface *screenSurface = NULL;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n",
+		       SDL_GetError());
+	} else {
+		//Create window
+		window = SDL_CreateWindow("tbsprpg sdl",
+					  SDL_WINDOWPOS_UNDEFINED,
+					  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+					  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL) {
+			printf("Window could not be created! SDL_Error: %s\n",
+			       SDL_GetError());
+		} else {
+			//Get window surface
+			screenSurface = SDL_GetWindowSurface(window);
+
+			//Fill the surface white
+			SDL_FillRect(screenSurface, NULL,
+				     SDL_MapRGB(screenSurface->format, 0xFF,
+						0xFF, 0xFF));
+
+			//Update the surface
+			SDL_UpdateWindowSurface(window);
+
+			//Hack to get window to stay up
+			SDL_Event e;
+			bool quit = false;
+			while (quit == false) {
+				while (SDL_PollEvent(&e)) {
+					if (e.type == SDL_QUIT)
+						quit = true;
+				}
+			}
+		}
+	}
+
+	//Destroy window
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
 
 	inicfg_close();
 
