6fd41998a7d4ad1284ca54cd7e10819c9a0a5527
cdvanhorn <cdavid.vanhorn@gmail.com>
2024-08-29 19:36:50 -0400

    feature/game_loop
    Removed unused commented out code.

--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 
 #include <stddef.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
 static void config_game(struct game_cfg *cfg)
 {
 	inicfg_getstring("game", "core", &(cfg->core));
@@ -40,39 +37,3 @@ int main(int argc, char *argv[])
 	log_write(LOG_TAG_INFO, "logger destroyed");
 	return 0;
 }
-
-// int run_game(int simfps)
-// {
-// 	srand(time(NULL));
-// 	// how big of steps take to run simulation at 25fps
-// 	uint32_t timestep = 1000 / simfps;
-// 	// most simulation updates to run before rendering
-// 	int max_sim_runs = 5;
-//
-// 	int sim_runs;
-// 	float interpolation;
-// 	uint64_t curtime = get_tick_count();
-//
-// 	int bigloops = 0;
-//
-// 	bool running = true;
-// 	while (running && bigloops < 100) {
-// 		sim_runs = 0;
-// 		printf("Start %d %" PRIu64 " %" PRIu64 "\n", sim_runs, get_tick_count(), curtime);
-// 		while (get_tick_count() > curtime && sim_runs < max_sim_runs) {
-// 			// update the game
-// 			Sleep((rand() % 2) + 1);
-// 			curtime += timestep;
-// 			sim_runs++;
-// 			printf("Sim %d %" PRIu64 " %" PRIu64 "\n", sim_runs, get_tick_count(), curtime);
-// 		}
-//
-// 		interpolation = (float)(get_tick_count() + timestep - curtime) /
-// 				(float)timestep;
-// 		printf("%.2f\n", interpolation);
-// 		//render(interpolation);
-// 		Sleep((rand() % 30) + 1);
-// 		bigloops++;
-// 	}
-// 	// printf("%" PRIu64 "\n", get_tick_count());
-// }
