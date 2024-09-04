19d6a8cd001b79f2c9aeeed22192f6c7efb02961
cdvanhorn <cdavid.vanhorn@gmail.com>
2024-09-03 20:15:59 -0400

    feature/load_png
    Installed SDL2_image

--- render/sdl/render_sdl.c
+++ render/sdl/render_sdl.c
@@ -13,7 +13,7 @@ static SDL_Renderer *renderer = NULL;
 int render_sdl_init(struct render_cfg *cfg)
 {
 	window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED,
-				  SDL_WINDOWPOS_UNDEFINED, 640, 480,
+				  SDL_WINDOWPOS_UNDEFINED, 800, 600,
 				  SDL_WINDOW_SHOWN);
 	if (window == NULL) {
 		printf("Window could not be created! SDL Error: %s\n",
@@ -39,7 +39,7 @@ int render_sdl_frame(struct render_frame *frame, float interpolation)
 {
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
-	SDL_Rect fillRect = { 640 / 4, 480 / 4, 640 / 2, 480 / 2 };
+	SDL_Rect fillRect = { 800 / 4, 600 / 4, 800 / 2, 600 / 2 };
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
 	SDL_RenderFillRect(renderer, &fillRect);
 	SDL_RenderPresent(renderer);
